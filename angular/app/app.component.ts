ea1782e42120c09662b90aad1c73c2f41d0073c5
Enoch Kumala <Enoch.Kumala@childplus.com>
2018-05-18 09:08:48 -0400

    backgrounds

--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MyListComponent } from './mylist/mylist.component'
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export class Data {
   text: string;
@@ -24,12 +25,13 @@ export class AppComponent {
   url: string = "http://www.google.com"
   data: any[];
   thingy: Date = new Date();
-  src: string = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_inbody_900x506%2Fpublic%2F1502824044%2Froyal-farms-best-gas-station-food-FT-SS0817.jpg%3Fitok%3Dig79fdSU&w=700&q=85"
+  src: any;
+  color: string = "black"
   style: any;
 
   checked: boolean;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.data = [{text: "numero uno", target: "http://google.com"},
                  {text: "numero due", target: "http://yahoo.com"},
                  {text: "numero tre", target: "https://duckduckgo.com/"},
@@ -39,9 +41,10 @@ export class AppComponent {
     //              new Data("numero tre", "https://duckduckgo.com/"),
     //              new Data("numero quattro", "https://bing.com/")];
 
+    this.src = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_inbody_900x506%2Fpublic%2F1502824044%2Froyal-farms-best-gas-station-food-FT-SS0817.jpg%3Fitok%3Dig79fdSU&w=700&q=85";
     this.style = {
-      'filter': 'brightness(50%)'
-    };
+      'background-image' : 'url(' + this.src + ')',
+      'height' : '100%' };
   }
 
   click(event: any) {
